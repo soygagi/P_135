8de19879ba9d4d987988875ad5ef13b2c9e42632
soygagi <101847092+soygagi@users.noreply.github.com>
2023-01-24 19:41:48 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+object = [];
 video = "";
 status = "";
 function preload() {
@@ -9,7 +10,20 @@ function setup(){
     canvas.center();
 }
 function draw(){
-    image(video, 0,0,480.380)
+    image(video, 0,0,480.380);
+    if(status!=""){
+        objectDetector.detect(video,gotResult);
+        for (var i=0;i<object.length;i++){
+            document.getElementById("status").innerHTML = "Estado: objetos detectados";
+            document.getElementById("numero_de_objetos").innerHTML = "Numero de objetos detectados"+object.length;
+            fill("#8A0868")
+            percent = floor(object[i].confidence*100);
+            text(object[i].label+""+percent+"%",object[1].x+15, object[i].y+15);
+            nofill();
+            stroke("white");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }    
+    }
 }
 function star(){
     objectDetector = ml5.objectDetector('cocossd' ,modelLoaded);
@@ -21,4 +35,11 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
 }
